4042f357ca2c0d5962b5ef4ade901a35d372b9c8
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-03-29 13:19:21 -0700

    Intents
    Definir intent explicito para calcular edad con manejo de errores

--- app/src/main/java/com/cochis/huevitoconcatsup/IntentsActivity.kt
+++ app/src/main/java/com/cochis/huevitoconcatsup/IntentsActivity.kt
@@ -4,6 +4,12 @@ import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.lang.Exception
+import java.lang.NumberFormatException
+import java.util.*
 
 class IntentsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,28 @@ class IntentsActivity : AppCompatActivity() {
         var btnCalcularEdad = findViewById<Button>(R.id.buttonIntentsCalculateAge)
         btnCalcularEdad.setOnClickListener {
             // TODO: add code to calculate age
+            try {
+                val añoDeNacimiento =
+                    findViewById<EditText>(R.id.editTextIntentsYearOfBirth).text.toString()
+                if (añoDeNacimiento.isNullOrEmpty()) {
+                    throw IllegalArgumentException("Cadena vacía")
+                }
+                val añoActual = Calendar.getInstance().get(Calendar.YEAR)
+                val edad = añoActual - añoDeNacimiento.toInt()
+                var tuEdadEs = findViewById<TextView>(R.id.textViewIntentsEdad)
+                tuEdadEs.text = "Tu edad es: ${edad.toString()}"
+            } catch (ex: IllegalArgumentException) {
+                Toast.makeText(this,
+                    "El año es obligatorio, escribelo c...",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } catch (ex: NumberFormatException) {
+                Toast.makeText(this,
+                    "Valor de año incorrecto",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
         }
 
         var btnSiguiente = findViewById<Button>(R.id.buttonIntentsNext)
